002be9b5712177a2d869152365894756eaf2d29c
FABIO ALEXANDRE FISCHER <fabio.fischer@alunos.sc.senac.br>
2025-08-11 21:23:42 -0300

    commit test6

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-        </a>
+        <a href="https://vite.dev" target="_blank"></a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>My first page in github pages -  Fabio Fischer </h2>
+      <h2>My first page in github pages - Fabio Fischer </h2>
       <h3>Um texto topper</h3>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+        alt="Logo React"
+      />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
